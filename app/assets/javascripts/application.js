46425fecbf3f84f7da239f388b8fe6934df82713
John Roberts <john.calvin.roberts@gmail.com>
2017-03-26 16:11:24 +0800

    clock working. need to add projects model and sorting logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,8 +26,33 @@ $(function () {
 });
 
 
-$(document).ready(function () {
-    setInterval(function () {
-        $('.time-rect-container').load('application/give_time');
-    }, 1000);
+function updateClock(){
+    var currentTime = new Date ( );
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
+    var currentSeconds = currentTime.getSeconds ( );
+
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+    // Choose either "AM" or "PM" as appropriate
+    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+    // Convert the hours component to 12-hour format if needed
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+    // Convert an hours component of "0" to "12"
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+    // Compose the string for display
+    var formatted = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+
+  $('.time-rect-container').html(formatted);
+}
+
+$(document).ready(function() {
+    setInterval("updateClock()",1000);
 });
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def give_time
-    @time = Time.now.strftime("%H:%M:%S ")
-    render :partial => 'shared/time_portion'
-  end
-
 end
